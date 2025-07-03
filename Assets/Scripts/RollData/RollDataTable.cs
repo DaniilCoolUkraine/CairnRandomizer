24e3d9d43881b9ab24d515bd5d17e2120538b6f2
DaniilCoolUkraine <cd0games2000@gmail.com>
2025-07-03 11:14:08 +0300

    finished data gathering

--- Assets/Scripts/RollData/RollDataTable.cs
+++ Assets/Scripts/RollData/RollDataTable.cs
@@ -1,11 +1,17 @@
 ﻿using CairnRandomizer.RollData.Equipment;
+using CairnRandomizer.RollData.Names;
+using CairnRandomizer.RollData.Traits;
 using UnityEngine;
 
 namespace CairnRandomizer.RollData
 {
+    [CreateAssetMenu(fileName = nameof(RollDataTable), menuName = "RollData/_"+nameof(RollDataTable))]
     public class RollDataTable : ScriptableObject
     {
-        [SerializeField] private EquipmentDataTable _type;
-        
+        public EquipmentDataTable EquipmentDataTable;
+        public MaleNameDataTable MaleNameDataTable;
+        public FemaleNameDataTable FemaleNameDataTable;
+        public SurnameDataTable SurnameDataTable;
+        public TraitsDataTable TraitsDataTable;
     }
 }
